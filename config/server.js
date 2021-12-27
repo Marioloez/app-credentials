40da5a6bdb0029e92f4fcbb3393de37b49d32b8e
Angel Morales R <kisamenarusas@gmail.com>
2021-12-27 08:32:30 -0600

    microsoft authv2

--- config/server.js
+++ config/server.js
@@ -1,9 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("", "https://app-credential.herokuapp.com"),
+  /*   url: env("", "https://e0f9-177-227-166-41.ngrok.io"), */
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b79e0cbbcdc04f9a21c0d2a571b5e6c2'),
+      secret: env("ADMIN_JWT_SECRET", "b79e0cbbcdc04f9a21c0d2a571b5e6c2"),
     },
   },
 });
+
+/* module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("", "https://e0f9-177-227-166-41.ngrok.io"),
+});
+ */
