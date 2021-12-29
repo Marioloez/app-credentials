6f840180ffe7fdfd083410322be61ddd8dd35999
Marioloez <48298718+Marioloez@users.noreply.github.com>
2021-12-29 17:10:22 -0600

    port-4000

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", 4000),
   url: env("", "https://app-credential.herokuapp.com"),
   /*   url: env("", "https://e0f9-177-227-166-41.ngrok.io"), */
   admin: {
